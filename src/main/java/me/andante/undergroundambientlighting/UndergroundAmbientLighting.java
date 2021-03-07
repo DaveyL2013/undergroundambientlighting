fb09c2e4d63ce5caba1245cf8e59d1b092d04768
DaveyL2013 <20153257+DaveyL2013@users.noreply.github.com>
2021-03-06 21:14:13 -0500

    Update UndergroundAmbientLighting.java

--- src/main/java/me/andante/undergroundambientlighting/UndergroundAmbientLighting.java
+++ src/main/java/me/andante/undergroundambientlighting/UndergroundAmbientLighting.java
@@ -2,12 +2,33 @@ package me.andante.undergroundambientlighting;
 
 import me.andante.undergroundambientlighting.config.UALConfigManager;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.IOException;
 
 public class UndergroundAmbientLighting implements ClientModInitializer {
     public static final String MOD_ID = "undergroundambientlighting";
 
     @Override
     public void onInitializeClient() {
-        UALConfigManager.load();
-    }
+		String FILEPATH = FabricLoader.getInstance().getConfigDir().toFile().toPath().resolve(MOD_ID + ".json").toString();
+		File FILE = new File(FILEPATH);
+		try {
+			if (FILE.createNewFile()) {
+				try (PrintWriter out = new PrintWriter(FILEPATH)) {
+					out.println("{\"enabled\":true,\"start_y\":63,\"end_y\":32,\"intensity\":0.032}");
+				} catch (IOException e) {
+					
+					}
+			}
+			
+		} catch (IOException e) {
+			
+			}
+		UALConfigManager.load();
+		
+	}	
+
 }
